5ba9922ac5e9ce90ae50418e5ecb36fcb868a596
AlbyReji <albymathew30@gmail.com>
2023-07-24 15:03:57 +0530

    Database settings

--- blogging_platform/blogging_platform/settings.py
+++ blogging_platform/blogging_platform/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'blogging_platform.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blogdb',
+        'USER':'root',
+        'PASSWORD': 'alby1234',
+        'HOST':'localhost',
+        'PORT':'3306'
     }
 }
 
